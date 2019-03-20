6a96729993d64d84f7f277d3fdfe18fa65d32cf1
Facundo Díaz <47418339+FacuDPascielli@users.noreply.github.com>
2019-03-20 11:46:07 -0300

    Add files via upload

--- Laboratorio/main.c
+++ Laboratorio/main.c
@@ -3,6 +3,45 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int hora;
+
+    printf("Ingrese una hora\n");
+    scanf("%d", &hora);
+
+    switch(hora)
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+        printf("A dormir\n");
+        break;
+    case 7:
+    case 8:
+        printf("Buenos dias \n");
+        break;
+    case 10:
+    case 11:
+    case 12:
+    case 13:
+    case 14:
+    case 15:
+    case 16:
+    case 17:
+    case 18:
+    case 19:
+    case 20:
+    case 21:
+    case 22:
+    case 23:
+            printf("Buenas noches\n");
+            break;
+    default:
+        printf("No es una hora valida");
+    }
+
     return 0;
 }
